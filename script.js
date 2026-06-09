60cf169095b8381fb7ee9cb041c6308bd3a95e72
Fazlur Rahmatullah <ajulrahmatullah@gmail.com>
2026-06-09 14:00:57 +0700

    Pull terbaru from local to github

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Fungsi untuk menampilkan pesan pada bagian kontak *soon hapus saat upload
 function tampilkanPesan() {
     const pesan = document.getElementById("pesan");
 
     pesan.innerHTML = "Terima kasih sudah mengunjungi halaman CV saya! untuk detailnya bisa click 'Link Terkait'";
 }
 
-// Efek sederhana saat halaman selesai dimuat *soon hapus saat upload
 window.onload = function() {
     console.log("Website CV berhasil dimuat.");
 };
